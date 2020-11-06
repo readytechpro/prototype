6948ecb9a31f7a7a22b8a750d8f40a837fe652f5
Grant King <grantryan.king@gmail.com>
2020-11-05 21:13:18 -0800

    Add CopyText model
    Add first model for holding copy text for the site.  Register model with admin, make migrations, and update database.

--- mysite/sitecore/admin.py
+++ mysite/sitecore/admin.py
@@ -1,3 +1,4 @@
 from django.contrib import admin
+from .models import CopyText
 
-# Register your models here.
+admin.site.register(CopyText)
--- mysite/sitecore/models.py
+++ mysite/sitecore/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class CopyText(models.Model):
+    label = models.CharField(max_length=25)
+    text = models.TextField()
+
+    def __str__(self):
+        return f'{self.label}: {self.text[:10]}'
+        
