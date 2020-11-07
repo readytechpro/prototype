faa4aa3c8f715bc315fdb41f0cb76e8c00f4515e
Grant King <grantryan.king@gmail.com>
2020-11-06 19:05:05 -0800

    Add FrequentQuestion model
    Add FrequentQuestion model that inherits from CopyText. Register the model with admin, make migrations, and update database.

--- mysite/sitecore/admin.py
+++ mysite/sitecore/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
-from .models import CopyText
+from .models import CopyText, FrequentQuestion
 
 admin.site.register(CopyText)
+admin.site.register(FrequentQuestion)
--- mysite/sitecore/models.py
+++ mysite/sitecore/models.py
@@ -6,4 +6,16 @@ class CopyText(models.Model):
 
     def __str__(self):
         return f'{self.label}: {self.text[:10]}'
+
+
+class FrequentQuestion(CopyText):
+    question_text = models.CharField(max_length=150)
+    votes = models.IntegerField()
+
+    @property
+    def answer_text(self):
+        return self.text
+
+    def __str__(self):
+        return f'{self.precedence} - {self.label}: {self.text[:10]}'
         
