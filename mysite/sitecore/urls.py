40931f0b7bcdb7dc96a37f59a84f5ec45fdcdb19
Grant King <grantryan.king@gmail.com>
2020-11-06 19:56:34 -0800

    Add view for displaying FAQs
    Add ListView class view for displaying FAQs. Add route. Add template.

--- mysite/sitecore/urls.py
+++ mysite/sitecore/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from . import views
+from .views import FAQListView
 
 urlpatterns = [
-    path('', views.index, name='index')
+    path('', views.index, name='index'),
+    path('faq/', FAQListView.as_view(), name='faq')
 ]
--- mysite/sitecore/views.py
+++ mysite/sitecore/views.py
@@ -1,6 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.views.generic import ListView
+from .models import FrequentQuestion
 
 def index(request):
     return render(request, 'sitecore/index.html')
 
+class FAQListView(ListView):
+    model = FrequentQuestion
+    template_name = 'sitecore/faq.html'
+    context_object_name = 'faqs'
+    ordering = ['votes']
