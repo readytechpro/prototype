67f74e084423834aa0cc5851d06d470a95904383
Grant King <grantryan.king@gmail.com>
2020-11-17 18:18:46 -0800

    Add view for Resources about page
    Add about Resources program to views.py that will retrieve and render the appropriate CopyText object. Add placeholder object to database.
    
    Add route to urls.py.
    
    Create minimal template for all similar views for this and other program about pages.

--- mysite/sitecore/urls.py
+++ mysite/sitecore/urls.py
@@ -4,5 +4,6 @@ from .views import FAQListView
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('faq/', FAQListView.as_view(), name='faq')
+    path('faq/', FAQListView.as_view(), name='faq'),
+    path('resources/', views.about_resources_program, name='about-resources')
 ]
--- mysite/sitecore/views.py
+++ mysite/sitecore/views.py
@@ -1,10 +1,16 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.views.generic import ListView
-from .models import FrequentQuestion
+from .models import FrequentQuestion, CopyText
 
 def index(request):
     return render(request, 'sitecore/index.html')
+
+def about_resources_program(request):
+    about_copy = CopyText.objects.get(label='resources')
+    context = {'about_copy': about_copy}
+    return render(request, 'sitecore/about_program.html', context)
+
 
 class FAQListView(ListView):
     model = FrequentQuestion
