15bbbbcd2614476a91787b3f9a711504321dedea
Grant King <grantryan.king@gmail.com>
2020-11-17 18:10:22 -0800

    Update CopyText model
    Update CopyText model to include title field and update database.

--- mysite/sitecore/models.py
+++ mysite/sitecore/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 
 class CopyText(models.Model):
+    title = models.CharField(max_length=50)
     label = models.CharField(max_length=25)
     text = models.TextField()
 
