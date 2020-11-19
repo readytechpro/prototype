8bbadd0f7c3e75f1998c6794989806957b7ce258
Grant King <grantryan.king@gmail.com>
2020-11-18 18:16:47 -0800

    Add views for remaining program about pages
    Add functions to views.py for remaining three program about pages: about_community_program, about_collaboration_program,  and about_opportunities_program.
    
    Add placeholder CopyText objects to database for these pages.
    
    Add routes for about pages to urls.py

--- mysite/sitecore/urls.py
+++ mysite/sitecore/urls.py
@@ -5,5 +5,8 @@ from .views import FAQListView
 urlpatterns = [
     path('', views.index, name='index'),
     path('faq/', FAQListView.as_view(), name='faq'),
-    path('resources/', views.about_resources_program, name='about-resources')
+    path('resources/', views.about_resources_program, name='about-resources'),
+    path('community/', views.about_community_program, name='about-community'),
+    path('collaboration/', views.about_collaboration_program, name='about-collaboration'),
+    path('opportunities/', views.about_opportunities_program, name='about-opportunities'),
 ]
--- mysite/sitecore/views.py
+++ mysite/sitecore/views.py
@@ -11,6 +11,21 @@ def about_resources_program(request):
     context = {'about_copy': about_copy}
     return render(request, 'sitecore/about_program.html', context)
 
+def about_community_program(request):
+    about_copy = CopyText.objects.get(label='community')
+    context = {'about_copy': about_copy}
+    return render(request, 'sitecore/about_program.html', context)
+
+def about_collaboration_program(request):
+    about_copy = CopyText.objects.get(label='collaboration')
+    context = {'about_copy': about_copy}
+    return render(request, 'sitecore/about_program.html', context)
+
+def about_opportunities_program(request):
+    about_copy = CopyText.objects.get(label='opportunities')
+    context = {'about_copy': about_copy}
+    return render(request, 'sitecore/about_program.html', context)
+
 
 class FAQListView(ListView):
     model = FrequentQuestion
