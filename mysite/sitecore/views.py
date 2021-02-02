36448b6e6e08056ee1430f5788801e68032601c5
Grant King <grantryan.king@gmail.com>
2021-02-02 00:56:40 -0800

    Update, refactor views to DRY
    Refactor views.py to remove repetitious about program views. Replace explicit page views with a generalized view that uses an argument from the URL to filter the appropriate objects and render them within the generalized about_program.html template.
    
    Update view to pass Page and related group of CopyText objects in context to the template.
    
    Update urls.py with corresponding changes.

--- mysite/sitecore/views.py
+++ mysite/sitecore/views.py
@@ -1,29 +1,15 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.views.generic import ListView
-from .models import FrequentQuestion, CopyText
+from .models import FrequentQuestion, CopyText, Page
 
 def index(request):
     return render(request, 'sitecore/index.html')
 
-def about_resources_program(request):
-    about_copy = CopyText.objects.get(label='resources')
-    context = {'about_copy': about_copy}
-    return render(request, 'sitecore/about_program.html', context)
-
-def about_community_program(request):
-    about_copy = CopyText.objects.get(label='community')
-    context = {'about_copy': about_copy}
-    return render(request, 'sitecore/about_program.html', context)
-
-def about_collaboration_program(request):
-    about_copy = CopyText.objects.get(label='collaboration')
-    context = {'about_copy': about_copy}
-    return render(request, 'sitecore/about_program.html', context)
-
-def about_opportunities_program(request):
-    about_copy = CopyText.objects.get(label='opportunities')
-    context = {'about_copy': about_copy}
+def about_program(request, program_name):
+    page = Page.objects.get(label=program_name)
+    copy_items = CopyText.objects.filter(page__label=program_name).order_by('position')
+    context = {'segments': copy_items, 'page': page}
     return render(request, 'sitecore/about_program.html', context)
 
 
