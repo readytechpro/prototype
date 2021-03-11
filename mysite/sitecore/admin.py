6ea4b083e00daf0121f671550f5ca3cd68eb3d55
Grant King <grantryan.king@gmail.com>
2021-03-11 03:45:08 -0800

    Update admin.py
    Register SectionTitle model.

--- mysite/sitecore/admin.py
+++ mysite/sitecore/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import CopyText, FrequentQuestion, Page
+from .models import CopyText, FrequentQuestion, Page, SectionTitle
 
 admin.site.register(Page)
 admin.site.register(CopyText)
 admin.site.register(FrequentQuestion)
+admin.site.register(SectionTitle)
