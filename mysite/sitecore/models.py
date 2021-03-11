71759a402c5261db537f103cccbcc8ace9d8877a
Grant King <grantryan.king@gmail.com>
2021-03-11 03:40:13 -0800

    Add, update models
    Add SectionTitle model to setup a relationship that allows for many realted CopyText per SectionTitle, and many SectionTitles per Page.  Modify CopyText and Page for this new schema.
    
    Add automatic slug filed creation functionality to Page model.

--- mysite/sitecore/models.py
+++ mysite/sitecore/models.py
@@ -1,22 +1,36 @@
 from django.db import models
+from django.utils.text import slugify
 
 class Page(models.Model):
-    label = models.CharField(max_length=50)
     title = models.CharField(max_length=50)
+    label = models.SlugField(max_length=50, unique=True, null=True, blank=True)
+
+    def save(self, *args, **kwargs):
+        if not self.label:
+            self.label = slugify(self.title)
+        super(Page, self).save(*args, **kwargs)
 
     def __str__(self):
         return f'{self.label}'
 
 
-class CopyText(models.Model):
+class SectionTitle(models.Model):
     page = models.ForeignKey(Page, on_delete=models.CASCADE, null=True)
-    label = models.CharField(max_length=50)
-    title = models.CharField(max_length=50)
-    text = models.TextField()
-    position = models.IntegerField(default=0)
+    title = models.CharField(max_length=100)
+    position = models.IntegerField(default=0) #position of section witin page
 
     def __str__(self):
-        return f'{self.page.label}: {self.position}: {self.title[:10]}: {self.text[:7]}'
+        return f'{self.title} ({self.page}, position {self.position})'
+
+
+class CopyText(models.Model):
+    title = models.ForeignKey(SectionTitle, on_delete=models.CASCADE, null=True)
+    text = models.TextField()
+    position = models.IntegerField(default=0) #position of copy within section
+
+    def __str__(self):
+        
+        return f'{self.text[:30]}, {self.title}'
 
 
 class FrequentQuestion(models.Model):
