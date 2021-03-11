a007bf33cc5a3736f6e3500fda2fb9282ea234d4
Grant King <grantryan.king@gmail.com>
2021-03-11 03:44:25 -0800

    Update about_program view
    Update about_program view and template to work with new and updated models.
    
    Update template layout.

--- mysite/sitecore/views.py
+++ mysite/sitecore/views.py
@@ -1,15 +1,20 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.views.generic import ListView
-from .models import FrequentQuestion, CopyText, Page
+from .models import FrequentQuestion, CopyText, Page, SectionTitle
 
 def index(request):
     return render(request, 'sitecore/index.html')
 
 def about_program(request, program_name):
     page = Page.objects.get(label=program_name)
-    copy_items = CopyText.objects.filter(page__label=program_name).order_by('position')
-    context = {'segments': copy_items, 'page': page}
+    section_structures = []
+    sections = SectionTitle.objects.filter(page__label=program_name).order_by('position')
+    for section in sections:
+        section_copy_items = CopyText.objects.filter(title=section).order_by('position')
+        section_structures.append((section, section_copy_items))
+    
+    context = {'sections': section_structures, 'page': page}
     return render(request, 'sitecore/about_program.html', context)
 
 
