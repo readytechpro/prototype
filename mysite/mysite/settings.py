69641fb63c3c83847bb89bcf0d800dcc7e32e592
Grant King <grantryan.king@gmail.com>
2021-03-12 01:09:14 -0800

    Update settings
    Add new Volunteering app.

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'sitecore.apps.SitecoreConfig',
+    'volunteering.apps.VolunteeringConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
