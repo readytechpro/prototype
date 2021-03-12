b4f3127b30eeb02fd8bbc1df3cfeffe5e73bb78d
Grant King <grantryan.king@gmail.com>
2021-03-12 01:12:41 -0800

    Update urls
    Add volunteering app path to project urls, and an index path to the app urls.

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -17,6 +17,7 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
+    path('volunteering/', include('volunteering.urls')),
     path('', include('sitecore.urls')),
     path('admin/', admin.site.urls),
 ]
