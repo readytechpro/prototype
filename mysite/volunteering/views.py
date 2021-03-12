861b389bd005f1b0861c958d0f251bc1f00b419f
Grant King <grantryan.king@gmail.com>
2021-03-12 01:15:46 -0800

    Add index view
    Add a generic list view for VolunteerProject objects. Add basic template.

--- mysite/volunteering/views.py
+++ mysite/volunteering/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.views.generic import ListView
+from .models import (
+    VolunteerProject, VolunteerProjectDetail, VolunteerProjectDetailCategory
+)
 
-# Create your views here.
+class ProjectListView(ListView):
+    model = VolunteerProject
+    template_name = 'volunteering/index.html'
+    context_object_name = 'projects'
