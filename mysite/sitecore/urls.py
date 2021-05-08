bbc80edfecc43141a0ad09f37809873415fa8058
Grant King <grantryan.king@gmail.com>
2021-05-07 20:55:02 -0700

    Fix spelling error
    Fix spelling error in about_program view route name argument.

--- mysite/sitecore/urls.py
+++ mysite/sitecore/urls.py
@@ -7,5 +7,5 @@ app_name = 'sitecore'
 urlpatterns = [
     path('', views.index, name='index'),
     path('faq/', FAQListView.as_view(), name='faq'),
-    path('about/<slug:program_name>/', views.about_program, name='about-program-detial'),
+    path('about/<slug:program_name>/', views.about_program, name='about-program-detail'),
 ]
