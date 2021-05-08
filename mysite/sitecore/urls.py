718ba5aa025c7d65941270e3c265da81749ec700
Grant King <grantryan.king@gmail.com>
2021-05-07 17:58:43 -0700

    Add app namespaces
    Add app_name variable to namespace sitecore and volunteering app routes.

--- mysite/sitecore/urls.py
+++ mysite/sitecore/urls.py
@@ -2,8 +2,10 @@ from django.urls import path
 from . import views
 from .views import FAQListView
 
+app_name = 'sitecore'
+
 urlpatterns = [
     path('', views.index, name='index'),
     path('faq/', FAQListView.as_view(), name='faq'),
-    path('about/<str:program_name>/', views.about_program, name='about-program-detial'),
+    path('about/<slug:program_name>/', views.about_program, name='about-program-detial'),
 ]
--- mysite/volunteering/urls.py
+++ mysite/volunteering/urls.py
@@ -2,6 +2,8 @@ from django.urls import path
 from . import views
 from .views import ProjectListView
 
+app_name = 'volunteering'
+
 urlpatterns = [
     path('', ProjectListView.as_view(), name='index')
 ]
