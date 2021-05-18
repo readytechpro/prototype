c083e5d55be5fc3bb9a4a2b295d79adbe068377e
Grant King <grantryan.king@gmail.com>
2021-05-17 22:48:16 -0700

    Add image resizing
    Add image resize functionality to Profile model save method.

--- mysite/users/models.py
+++ mysite/users/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
-
+from PIL import Image
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
@@ -8,3 +8,14 @@ class Profile(models.Model):
 
     def __str__(self):
         return f'{self.user.username} Profile'
+
+    def save(self, *args, **kwargs):
+        super().save(*args, **kwargs)
+
+        img = Image.open(self.image.path)
+        MAX_WIDTH = 300
+        if img.width > MAX_WIDTH:
+            resize_ratio = MAX_WIDTH / img.width
+            output_size = (int(img.width * resize_ratio), int(img.height * resize_ratio))
+            img = img.resize(output_size)
+            img.save(self.image.path)
