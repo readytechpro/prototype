37e44a2fb52631b3e6b9af269f5ae8a996a7b0ae
Grant King <grantryan.king@gmail.com>
2021-05-18 22:22:47 -0700

    Add user attribute to LearningResource model
    Add User foreign key attribute to LearningResource model so that these objects can be assigned to a particular user.
    
    Add migrations. Update database.
    
    Add get_absolute_url method to model so that it will return the route for a particular instance's detail page.

--- mysite/resources/models.py
+++ mysite/resources/models.py
@@ -1,9 +1,18 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.urls.base import reverse
 
 class LearningResource(models.Model):
     title = models.CharField(max_length=100)
     notion_link = models.URLField()
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
 
     def __str__(self):
-        return f'{self.title}'
+        if self.user:
+            return f'{self.title} - [{self.user.username}]'
+        else:
+            return f'{self.title}'
+
+    def get_absolute_url(self):
+        return reverse('resources:resource_detail', kwargs={'pk': self.pk})
 
