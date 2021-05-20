764d4e93efe6117e629ec2990977935fddc7ff72
Grant King <grantryan.king@gmail.com>
2021-05-19 18:12:54 -0700

    Add generic views to resources app
    Add UserLearningResourceListView, add route, and update the learning_resources_list template to render user-specific LearningResource objects. This view requires that the current user is the author of these objects.
    
    Add LearningResourceDetailView, add route, and add template for rendering an object's detail view. Template also renders link to edit view if the current user created the current object.
    
    Add generic editing views for the LearningResource model. Add CreateView, UpdateView, and DeleteView, add routes, and add templates to provide an interface for users to manage their LearningResource objects.

--- mysite/resources/urls.py
+++ mysite/resources/urls.py
@@ -1,10 +1,19 @@
 from django.urls import path
 from . import views
-from .views import LearningResourceListView
+from resources.views import (
+    LearningResourceListView, LearningResourceCreateView, 
+    LearningResourceUpdateView, LearningResourceDeleteView,
+    LearningResourceDetailView, UserLearningResourceListView,
+)
 
 app_name = 'resources'
 
 urlpatterns = [
     path('', views.index, name='index'), 
     path('all/', LearningResourceListView.as_view(), name='all_learning_resources'),
+    path('my_resources/', UserLearningResourceListView.as_view(), name='my_learning_resources'),
+    path('new/', LearningResourceCreateView.as_view(), name='resource_create'),
+    path('<int:pk>/', LearningResourceDetailView.as_view(), name='resource_detail'),
+    path('<int:pk>/update/', LearningResourceUpdateView.as_view(), name='resource_update'),
+    path('<int:pk>/delete/', LearningResourceDeleteView.as_view(), name='resource_delete' ),
 ]
--- mysite/resources/views.py
+++ mysite/resources/views.py
@@ -1,9 +1,13 @@
 from django.shortcuts import render
-from django.views.generic import ListView
+from django.urls import reverse_lazy
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from django.views.generic import (
+    DetailView, ListView, CreateView, UpdateView, DeleteView)
 from .models import LearningResource
 
 def index(request):
-    return render(request, 'resources/index.html')
+    context = {'user_tracks': LearningResource.objects.filter(user=request.user).count()}
+    return render(request, 'resources/index.html', context)
 
 
 class LearningResourceListView(ListView):
@@ -11,4 +15,73 @@ class LearningResourceListView(ListView):
     template_name = 'resources/learning_resources_list.html'
     context_object_name = 'learning_resources'
 
+
+class UserLearningResourceListView(LoginRequiredMixin, UserPassesTestMixin, ListView):
+    model = LearningResource
+    template_name = 'resources/learning_resources_list.html'
+    context_object_name = 'learning_resources'
+
+    def get_queryset(self):
+        return LearningResource.objects.filter(user=self.request.user)
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['user_resources'] = True
+        return context
+
+    def test_func(self):
+        queryset = self.get_queryset()
+        if queryset.first().user == self.request.user:
+            return True
+        else:
+            return False
+        
+
+class LearningResourceDetailView(LoginRequiredMixin, DetailView):
+    model = LearningResource
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['can_edit'] = self.is_author()
+        context['user_tracks'] = LearningResource.objects.filter(user=self.request.user).count()
+        return context
+
+    def is_author(self):
+        if self.get_object().user == self.request.user:
+            return True
+        else:
+            return False
+
+
+class LearningResourceCreateView(LoginRequiredMixin, CreateView):
+    model = LearningResource
+    fields = ['title', 'notion_link']
+
+    def form_valid(self, form):
+        form.instance.user = self.request.user
+        return super().form_valid(form)
+        
+    
+class LearningResourceUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
+    model = LearningResource
+    fields = ['title', 'notion_link']
+
+    def test_func(self):
+        if self.get_object().user == self.request.user:
+            return True
+        else:
+            return False
+
+
+class LearningResourceDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
+    model = LearningResource
+    
+    def get_success_url(self):
+        return reverse_lazy('resources:all_learning_resources')
+    
+    def test_func(self):
+        if self.get_object().user == self.request.user:
+            return True
+        else:
+            return False
 
