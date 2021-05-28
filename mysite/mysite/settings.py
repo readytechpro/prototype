ac6c74378e962081e140875d05c34a551fae8ee5
Grant King <grantryan.king@gmail.com>
2021-05-28 01:32:10 -0700

    Update to deploy
    Update settings, add files for deploying on GAE standard environment.
    
    Add requirements.txt, noxfile_config.py, main.py, app.yaml, and .gcloudignore files.
    
    Add settings so project will run on Google App Engine and use the GCP SQL databases. Project will use one of two similar databases, one for development and one for production.  Add settings variable that can be manually set when migrations need to be run on the production database.

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -28,8 +28,9 @@ SECRET_KEY = config['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
+MIGRATE_PRODUCTION_DB = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -82,13 +83,53 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+# Install PyMySQL as mysqlclient/MySQLdb to use Django's mysqlclient adapter
+# See https://docs.djangoproject.com/en/2.1/ref/databases/#mysql-db-api-drivers
+# for more information
+import pymysql  # noqa: 402
+pymysql.version_info = (1, 4, 6, 'final', 0)  # change mysqlclient version
+pymysql.install_as_MySQLdb()
 
+# [START db_setup]
+if os.getenv('GAE_APPLICATION', None) or MIGRATE_PRODUCTION_DB:
+    # Running on production App Engine, so connect to Google Cloud SQL using
+    # the unix socket at /cloudsql/<your-cloudsql-connection string>
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.mysql',
+            'HOST': f"/cloudsql/{config['YOUR-CONNECTION-NAME']}",
+            'USER': f"{config['YOUR-USERNAME']}",
+            'PASSWORD': f"{config['YOUR-PASSWORD']}",
+            'NAME': f"{config['PRODUCTION-DATABASE']}",
+        }
+    }
+else:
+    # Running locally so connect to either a local MySQL instance or connect to
+    # Cloud SQL via the proxy. To start the proxy via command line:
+    #
+    #     $ cloud_sql_proxy -instances=[INSTANCE_CONNECTION_NAME]=tcp:3306
+    #
+    # See https://cloud.google.com/sql/docs/mysql-connect-proxy
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.mysql',
+            'HOST': '127.0.0.1',
+            'PORT': '3306',
+            'NAME': f"{config['DEVELOPMENT-DATABASE']}",
+            'USER': f"{config['YOUR-USERNAME']}",
+            'PASSWORD': f"{config['YOUR-PASSWORD']}",
+        }
+    }
+# [END db_setup]
+
+# Use a in-memory sqlite3 database when testing in CI systems
+if os.getenv('TRAMPOLINE_CI', None):
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3')
+        }
+    }
 
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
@@ -126,6 +167,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
