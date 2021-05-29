1d2f8f240e9d58d1b72c45f8d5e482b8da0b188f
Grant King <grantryan.king@gmail.com>
2021-05-28 19:27:21 -0700

    Add conditional debug to settings
    Add logic to conditionally set DEBUG variable to the appropriate value, depending on if the app is running in production or development.

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -27,7 +27,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = config['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+if os.getenv('GAE_APPLICATION', None):
+    DEBUG = False
+else:
+    DEBUG = True
 MIGRATE_PRODUCTION_DB = False
 
 ALLOWED_HOSTS = ['*']
