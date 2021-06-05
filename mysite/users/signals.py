1238ba347ab41bdae535d9e3f1c9a52de20a0938
Grant King <grantryan.king@gmail.com>
2021-06-04 17:59:48 -0700

    Add pre_save signal for Profile
    Add pre_save signal for Profile objects to keep object storage from retaining old files. This signal will delete the old profile image when a new image file is uploaded by the User.

--- mysite/users/signals.py
+++ mysite/users/signals.py
@@ -1,7 +1,8 @@
-from django.db.models.signals import post_save
+from django.db.models.signals import post_save, pre_save
 from django.contrib.auth.models import User #sender
 from django.dispatch import receiver
 from .models import Profile
+from django.core.files.storage import default_storage as storage
 
 @receiver(post_save, sender=User)
 def create_profile(sender, instance, created, **kwargs):
@@ -14,3 +15,28 @@ def create_profile(sender, instance, created, **kwargs):
 def save_profile(sender, instance, **kwargs):
     #save the profile every time a user is saved
     instance.profile.save()
+
+@receiver(pre_save, sender=Profile)
+def delete_old_image(sender, instance, **kwargs):
+    #delete the old profile image before saving a new one
+    #don't do anything when creating a new object
+    if instance._state.adding and not instance.pk: 
+        return False
+
+    #retrieve the current file
+    try:
+        old_file = storage.open(sender.objects.get(pk=instance.pk).image.name, "w")
+        if old_file.name == 'default.jpg': #don't replace the default image
+            old_file.close()
+            return False
+    except sender.DoesNotExist:
+        return False
+
+    #save new file, delete old file if it isn't the same file
+    new_file = storage.open(instance.image.name, "w")
+    if not old_file.name == new_file.name:
+        storage.delete(old_file.name)
+        new_file.close()
+    else:
+        old_file.close()
+        new_file.close()
