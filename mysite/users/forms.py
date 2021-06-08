08a222ea1780f614ba7f2c18d677ce0c91a06978
Grant King <grantryan.king@gmail.com>
2021-06-07 22:33:21 -0700

    Add form image validation
    Add ProfileUpdateForm.clean_image() method to the ModelForm in order to prevent large images from being temporarily uploaded to Cloud storage.
    
    Although Profile.save() will resize all large images anyway, using validation here will preemptively check for extremely large files to prevent excessive cloud resource utilization.

--- mysite/users/forms.py
+++ mysite/users/forms.py
@@ -1,4 +1,5 @@
 from django import forms
+from django.core.exceptions import ValidationError
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
 from .models import Profile
@@ -23,3 +24,17 @@ class ProfileUpdateForm(forms.ModelForm):
     class Meta:
         model = Profile
         fields = ['image']
+
+    def clean_image(self):
+        MAX_BYTES = 6 * 1024 * 1024
+        image = self.cleaned_data['image']
+        file_size = image.size
+        if file_size > MAX_BYTES:
+            raise ValidationError(
+                f'Image too large to upload. The maximum upload size is \
+                    {(MAX_BYTES / 1024 / 1024):.2f} MB, and your \
+                    {(file_size / 1024 / 1024):.2f} MB image exceeds that \
+                    limit. Navigate to this page to try again.'
+                )
+        else:
+            return image
