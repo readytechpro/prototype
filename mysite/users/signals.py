5da40277434fa3134b0dafa307434d61ac704c34
Grant King <grantryan.king@gmail.com>
2021-06-09 01:58:11 -0700

    Add, update signals
    Add pre_save signal rename_file to check for and prevent duplicate filenames in storage.
    
    Update and refactor pre_save signal delete_old_image. Add a check to prevent deletion of another user's image for when two images could momentarily appear to have the same filename, and the wrong image would be deleted.

--- mysite/users/signals.py
+++ mysite/users/signals.py
@@ -1,3 +1,5 @@
+from pathlib import Path
+import os
 from django.db.models.signals import post_save, pre_save
 from django.contrib.auth.models import User #sender
 from django.dispatch import receiver
@@ -10,11 +12,22 @@ def create_profile(sender, instance, created, **kwargs):
     if created:
         Profile.objects.create(user=instance)
 
-
 @receiver(post_save, sender=User)
 def save_profile(sender, instance, **kwargs):
     #save the profile every time a user is saved
     instance.profile.save()
+
+@receiver(pre_save, sender=Profile)
+def rename_file(sender, instance, **kwargs):
+    #change instance filename if it already exists in database by another user
+    other_user_images = list(sender.objects.exclude(pk=instance.pk).values_list('image', flat=True))
+    if instance.image in other_user_images:
+        parent_directory = os.path.dirname(instance.image.name)
+        img_basename = os.path.basename(instance.image.name)
+        basename_split = list(os.path.splitext(img_basename))
+        basename_split[0] = f'{basename_split[0]}_{instance.user.pk}'
+        new_filename = os.path.join(parent_directory, ''.join(basename_split))
+        instance.image.name = new_filename
 
 @receiver(pre_save, sender=Profile)
 def delete_old_image(sender, instance, **kwargs):
@@ -25,18 +38,19 @@ def delete_old_image(sender, instance, **kwargs):
 
     #retrieve the current file
     try:
-        old_file = storage.open(sender.objects.get(pk=instance.pk).image.name, "w")
+        old_file = sender.objects.get(pk=instance.pk).image
         if old_file.name == 'default.jpg': #don't replace the default image
-            old_file.close()
             return False
     except sender.DoesNotExist:
         return False
 
-    #save new file, delete old file if it isn't the same file
-    new_file = storage.open(instance.image.name, "w")
-    if not old_file.name == new_file.name:
+    #exit if same filename is being used by another user
+    other_user_images = list(sender.objects.exclude(pk=instance.pk).values_list('image', flat=True))
+    if old_file in other_user_images:
+        return False
+
+    #delete old file if the form is about to save a different file
+    if not old_file.name == instance.image.name:
+        old_file = storage.open(old_file.name, 'w')
         storage.delete(old_file.name)
-        new_file.close()
-    else:
-        old_file.close()
-        new_file.close()
+        
