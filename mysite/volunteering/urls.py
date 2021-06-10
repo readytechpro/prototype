7eee5165d5e8186ca39289be0448fa958ddb4014
Grant King <grantryan.king@gmail.com>
2021-06-10 02:07:21 -0700

    Add pages, update index
    Add views, routes, and templates for minimal volunteer project list page and volunteer project detail page.
    
    Update index page. Now it just renders a link to the new project list page, also with the navigation inherited from the app's new base template.

--- mysite/volunteering/urls.py
+++ mysite/volunteering/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 from . import views
-from .views import ProjectListView
+from .views import ProjectListView, ProjectDetailView
 
 app_name = 'volunteering'
 
 urlpatterns = [
-    path('', ProjectListView.as_view(), name='index')
+    path('', views.index, name='index'),
+    path('all/', ProjectListView.as_view(), name='all_volunteer_projects'),
+    path('<int:pk>/', ProjectDetailView.as_view(), name='project_detail'),
 ]
--- mysite/volunteering/views.py
+++ mysite/volunteering/views.py
@@ -1,10 +1,21 @@
 from django.shortcuts import render
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 from .models import (
     VolunteerProject, VolunteerProjectDetail, VolunteerProjectDetailCategory
 )
 
+def index(request):
+    return render(request, 'volunteering/index.html')
+
+
 class ProjectListView(ListView):
     model = VolunteerProject
-    template_name = 'volunteering/index.html'
+    template_name = 'volunteering/volunteer_projects_list.html'
     context_object_name = 'projects'
+
+
+class ProjectDetailView(DetailView):
+    model = VolunteerProject
+    template_name = 'volunteering/volunteer_project_detail.html'
+    context_object_name = 'project'
+
