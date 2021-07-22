73d4a1cc9c50edb456c7d7c572319972b7847e03
Grant King <grantryan.king@gmail.com>
2021-07-21 20:19:44 -0700

    Add str methods
    Add __str__ methods to VolunteerProjectDetailCategory and VolunteerProjectDetail volunteering app models.

--- mysite/volunteering/models.py
+++ mysite/volunteering/models.py
@@ -13,8 +13,14 @@ class VolunteerProjectDetailCategory(models.Model):
     heading = models.CharField(max_length=150)
     position = models.IntegerField() # position of category within project post
 
+    def __str__(self):
+        return f'Category {self.position} "{self.heading}"- {self.project}'
+
 
 class VolunteerProjectDetail(models.Model):
     detail_category = models.ForeignKey(VolunteerProjectDetailCategory, on_delete=models.CASCADE)
     text = models.TextField()
     position = models.IntegerField() # position of detail within category
+
+    def __str__(self):
+        return f'Detail {self.position} - {self.detail_category}'
