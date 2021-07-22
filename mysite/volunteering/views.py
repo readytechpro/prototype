be56f5a0c3827304745c8266ec8dd782afa858f3
Grant King <grantryan.king@gmail.com>
2021-07-21 20:21:33 -0700

    Add detail page
    Add view and template for volunteer project detail page for the volunteering app.

--- mysite/volunteering/views.py
+++ mysite/volunteering/views.py
@@ -19,3 +19,17 @@ class ProjectDetailView(DetailView):
     template_name = 'volunteering/volunteer_project_detail.html'
     context_object_name = 'project'
 
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        project = self.get_object()
+        related_detail_categories = VolunteerProjectDetailCategory.objects.filter(
+            project=project).order_by('position')
+        details_structure = {}
+        for category in related_detail_categories:
+            related_details = VolunteerProjectDetail.objects.filter(
+                detail_category=category).order_by('position').values_list('text', flat=True)
+            details_structure[f'{category.heading}'] = related_details
+
+        context['categories_detials'] = details_structure
+        return context
+
