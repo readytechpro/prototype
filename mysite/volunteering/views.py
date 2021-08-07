a787c1897e501477a87a3c3a8b9f0724e2155e50
Grant King <grantryan.king@gmail.com>
2021-08-07 04:00:13 -0700

    Add dynamic color functionality, update styles
    Add dynamic color functionality to ProjectListView.
    
    Add get_context_data and get_colors methods to generate a list of similar colors to be sent to the template for list item backgrounds. The list of colors is the same length as the queryset.
    
    Add styled header.
    
    Update styles in listing area. Add functionality from color context variable.

--- mysite/volunteering/views.py
+++ mysite/volunteering/views.py
@@ -1,3 +1,4 @@
+import random
 from django.shortcuts import render
 from django.views.generic import ListView, DetailView
 from .models import (
@@ -12,6 +13,29 @@ class ProjectListView(ListView):
     model = VolunteerProject
     template_name = 'volunteering/volunteer_projects_list.html'
     context_object_name = 'projects'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        colors = self.get_colors(len(context['projects']))
+
+        context['projects'] = zip(context['projects'], colors)
+        return context
+
+    def get_colors(self, list_length):
+        colors = []
+
+        for item in range(list_length):
+            green_color = [28, 153, 84]
+            variance_coeff = 0.15
+            new_color = []
+            for component in green_color:
+                value_range = [int(component - (component * variance_coeff)), int(component + (component * variance_coeff))]
+                random_value = random.randint(*value_range)
+                new_color.append(random_value)
+
+            colors.append(new_color)
+        return colors
+
 
 
 class ProjectDetailView(DetailView):
