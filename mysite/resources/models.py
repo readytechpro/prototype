d804e4d861b0b60fe705f403dc94f88b53312811
Grant King <grantryan.king@gmail.com>
2021-08-11 07:03:53 -0700

    Update resources models
    Add tagline, description, and tag fields to LearningResource model.
    
    Add CategoryTag model for relationship to tag field.
    
    Add migrations.

--- mysite/resources/models.py
+++ mysite/resources/models.py
@@ -2,8 +2,19 @@ from django.db import models
 from django.contrib.auth.models import User
 from django.urls.base import reverse
 
+
+class CategoryTag(models.Model):
+    title = models.CharField(max_length=30)
+
+    def __str__(self):
+        return f'{self.title}'
+
+
 class LearningResource(models.Model):
     title = models.CharField(max_length=100)
+    tagline = models.CharField(max_length=255, default='Enter a tag line', null=True)
+    description = models.TextField(default='Enter a resource descripiton', null=True)
+    tags = models.ManyToManyField(CategoryTag)
     notion_link = models.URLField()
     user = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
 
