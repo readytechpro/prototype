f8081137bf85ce01a3ef8330027e70f3e6bf16ee
Grant King <grantryan.king@gmail.com>
2021-08-11 07:08:07 -0700

    update resources admin
    add CategoryTag to show in admin panel.

--- mysite/resources/admin.py
+++ mysite/resources/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
-from .models import LearningResource
+from .models import LearningResource, CategoryTag
 
 admin.site.register(LearningResource)
+admin.site.register(CategoryTag)
